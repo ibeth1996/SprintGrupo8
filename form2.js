e9101b95da2b8b5a7b0798356e749a47cb4cf2e9
CaroDGomez <caro5952@gmail.com>
2021-09-24 19:16:33 -0500

    Creación del archivo form2.js

---
+++ form2.js
@@ -0,0 +1,9 @@
+function validar_correo(){
+
+}
+function validar_contrasena_usuario(){
+
+}
+
+module.exports.validar_correo = validar_correo;
+module.exports.validar_contrasena_usuario = validar_contrasena_usuario;
